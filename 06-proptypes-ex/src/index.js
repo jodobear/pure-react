f346b18e7a9d18fa38762322a8cd6cbefbad67da
jodobear <jodobear@protonmail.com>
2020-01-15 14:46:20 +0100

    finished proptypes end of ch exercises

--- 06-proptypes-ex/src/index.js
+++ 06-proptypes-ex/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// address label
 function AddressLabel({ person }) {
   return (
     <div>
@@ -18,6 +19,7 @@ function AddressLabel({ person }) {
   )
 }
 
+// envelope
 function Stamp({ hash }) {
   const url = `https://www.gravatar.com/avatar/${hash}`;
   return (
@@ -59,6 +61,7 @@ const testPerson02 = {
   }
 }
 
+// credit card
 function Card({ cardInfo }) {
   return (
     <div className="card">
@@ -67,6 +70,9 @@ function Card({ cardInfo }) {
       </div>
       <div className="card-number">
         {cardInfo.number}
+      </div>
+      <div className="card-number-initials">
+        1234
       </div>
       <div className="valid-thru">
         VALID <br/> THRU
@@ -88,7 +94,67 @@ const testCard = {
   holder: "Rocking Windowlicker"
 }
 
+// poster
+
+function Poster({ image, title, text }) {
+  return (
+    <div className="poster">
+      <img className="poster-image" src={image} />
+      <h2 className="poster-title text">
+        {title}
+      </h2>
+      <p className="poster-text text">
+        {text}
+      </p>
+    </div>
+  )
+}
+const url = "https://miro.medium.com/max/360/1*jTT8EHj84BWnILq1A3MRqg.jpeg";
+const testPoster = {
+  image: url,
+  title: "Bitcoin",
+  text: "Magic Internet Money, come Join Us!"
+};
+
+// email
+function Email({ email }) {
+  return (
+    <div className="email">
+        <span className="email-select">
+          <i className="checkbox-light" />
+        </span>
+        <span className="email-pin">
+          <i className="fa-pinterest-p" />
+        </span>
+      <div className="email-info">
+        <h5 className="email-sender">
+          {email.sender.split(', ')[0]}
+        </h5>
+        <h5 className="email-subject">
+          {email.subject}
+        </h5>
+        <h6 className="email-date">
+          {email.date}
+        </h6>
+      </div>
+      <p className="email-message">
+        {email.message}
+      </p>
+    </div>
+  )
+}
+const msg = "Hello young Padawan! We have claimed self sovereignty of our money, ping us and we can show you the way out of this matrix."
+const testEmail = {
+  sender: "ABC, abc@mail.com",
+  subject: "The Pill",
+  date: "Jan 15",
+  message: msg
+}
+
 ReactDOM.render(
-[<Envelope fromPerson={testPerson01} toPerson={testPerson02} stamp={"abc"} />, <Card cardInfo={testCard} />],
+  [<Envelope fromPerson={testPerson01} toPerson={testPerson02} stamp={"abc"} />,
+    <Card cardInfo={testCard} />,
+    <Poster image={testPoster.image} title={testPoster.title} text={testPoster.text} />,
+    <Email email={testEmail} />],
   document.querySelector('#root')
 )
